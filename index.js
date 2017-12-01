a70da064d0033db4793ea99febba7fdde1415a31
Mubashshir Mohammed <39416@corp.hexaware.com>
2017-12-01 16:27:33 +0530

    First Draft

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+//dependencies
+// var calculator = require('./processor/calculator');
+
+app = express();
+//Create express object
+
+var port = process.env.PORT || 3000;
+//Assign port
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+//Configuring express app behaviour
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/api", function(req,res){
+  res.send("App works");
+})
+//GET Endpoint
+
+app.post("/api",function(req,res){
+  var body = req.body;
+res.send('Yay!');
+//   calculator.Concat(body,function(result){
+//     res.send(result);
+//   })//Callback Feature for Synced Calls
+})
+//POST Call Endpoint
+
+console.log("Server Running at Port : "+port);
+
+app.listen(port);
